4a1b704c84a4d0a561c04e78caedab694e84a23d
kirst tablet <k_e_smith412@yahoo.com>
2024-12-06 16:32:39 -0500

    hoping day6 works

--- 2024/day6_test.py
+++ 2024/day6_test.py
@@ -1,4 +1,4 @@
-with open('day6_in.txt') as fp:
+with open('day6_in_test.txt') as fp:
     lst = fp.read().split('\n')
 
 #print(lst)
@@ -37,7 +37,7 @@ def move_guard(current_i, current_j, current_direction, map_list,constant_ex_pos
 		elif map_list[current_i][current_j-1] == '#':
 			map_list[current_i][current_j] = '^'
 		elif map_list[current_i][current_j-1] == 'X':
-			constant_ex_positions.append((current_i,current_j-1]))
+			constant_ex_positions.append((current_i,current_j-1))
 			map_list[current_i][current_j-1] = '<'
 			map_list[current_i][current_j] = 'X'
 		else:
@@ -49,6 +49,10 @@ def move_guard(current_i, current_j, current_direction, map_list,constant_ex_pos
 			map_list[current_i][current_j] = 'X'
 		elif map_list[current_i - 1][current_j] == '#':
 			map_list[current_i][current_j] = '>'
+		elif map_list[current_i-1][current_j] == 'X':
+			constant_ex_positions.append((current_i-1,current_j))
+			map_list[current_i-1][current_j] = '^'
+			map_list[current_i][current_j] = 'X'
 		else:
 			map_list[current_i-1][current_j] = '^'
 			map_list[current_i][current_j] = 'X'
@@ -58,6 +62,10 @@ def move_guard(current_i, current_j, current_direction, map_list,constant_ex_pos
 			map_list[current_i][current_j] = 'X'
 		elif map_list[current_i][current_j+1] == '#':
 			map_list[current_i][current_j] = 'v'
+		elif map_list[current_i][current_j+1] == 'X':
+			constant_ex_positions.append((current_i,current_j+1))
+			map_list[current_i][current_j+1] = '>'
+			map_list[current_i][current_j] = 'X'
 		else:
 			map_list[current_i][current_j+1] = '>'
 			map_list[current_i][current_j] = 'X'
@@ -67,40 +75,74 @@ def move_guard(current_i, current_j, current_direction, map_list,constant_ex_pos
 			map_list[current_i][current_j] = 'X'
 		elif map_list[current_i +1][current_j] == '#':
 			map_list[current_i][current_j] = '<'
+		elif map_list[current_i+1][current_j] == 'X':
+			constant_ex_positions.append((current_i+1,current_j))
+			map_list[current_i+1][current_j] = 'v'
+			map_list[current_i][current_j] = 'X'
 		else:
 			map_list[current_i+1][current_j] = 'v'
 			map_list[current_i][current_j] = 'X'
-	return map_list
+	return map_list, constant_ex_positions
 	
-error_free = True
+#error_free = True
 
-while error_free:
-	lst = move_guard(curr_i,curr_j,curr_direction,lst)
-	curr_i,curr_j,curr_direction = get_guard_position(lst)
-	if curr_i is None or curr_j is None:
-		error_free = False
+#while error_free:
+	#lst = move_guard(curr_i,curr_j,curr_direction,lst)
+#	curr_i,curr_j,curr_direction = get_guard_position(lst)
+#	if curr_i is None or curr_j is None:
+#		error_free = False
 	
-print(lst)
-counter  = 0
-for row in lst:
-	for column in row:
-		if column == 'X':
-			counter += 1
+#print(lst)
+#counter  = 0
+#for row in lst:
+#	for column in row:
+#		if column == 'X':
+#			counter += 1
 			
-print(counter)
+#print(counter)
 
-constant_exes = 0
-
-temp_lst = lst.copy()
+#constant_exes = 0
 
 obstacle_i = 0
 obstacle_j = 0
 final_obstacle_list = []
 for obstacle_i in range(len(lst)-1):
-	for obstacle_j in range(len(lst[i]-1)):
+	for obstacle_j in range(len(lst[i])-1):
+		print("position:	{},{}".format(obstacle_i,obstacle_j))
+		
+		with open('day6_in_test.txt') as fp:
+		    temp_lst = fp.read().split('\n')
+		
+		#print(lst)
+		for i,row in enumerate(temp_lst):
+			temp_lst[i] = list(row)
+		
+		if temp_lst[obstacle_i][obstacle_j] in ['#','v','<','>','^']:
+			continue
 		constant_ex_positions = []
 		# change obstacle position
 		temp_lst[obstacle_i][obstacle_j] = '#'
 		error_free = True
 		ex_loop = False
+		prev_in_constant = False
+		# get starting index and direction
+		print('getting initial position...')
+		curr_i, curr_j, curr_direction = get_guard_position(temp_lst)
+		print(curr_i,curr_j,curr_direction)
+		print('starting while loop...')
+		while error_free:
+			temp_lst, constant_ex_positions = move_guard(curr_i,curr_j,curr_direction,temp_lst,constant_ex_positions)
+			curr_i,curr_j,curr_direction = get_guard_position(temp_lst)
+			if curr_i is None or curr_j is None:
+				error_free = False
+			# check if the current position index is in constant_ex_positions
+			if (curr_i,curr_j) in constant_ex_positions:
+				if not prev_in_constant:
+					prev_in_constant = True
+				else:
+					ex_loop = True
+		if ex_loop:
+			final_obstacle_list.append((obstacle_i,obstacle_j))
 			
+print(len(final_obstacle_list))
+print(final_obstacle_list)
